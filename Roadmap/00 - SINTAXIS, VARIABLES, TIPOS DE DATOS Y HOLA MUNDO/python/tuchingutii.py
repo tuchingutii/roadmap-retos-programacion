70079b420c51f3353e8ddf4c57ad872b8f10d196
Carlos Gutierrez <130489775+tuchingutii@users.noreply.github.com>
2024-01-04 14:57:04 -0300

    Create tuchingutii.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tuchingutii.py
@@ -0,0 +1,33 @@
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+#https://www.python.org/
+
+#Esto es un comentario en una linea
+
+"""
+Esto es un comentario
+en 
+varias lineas
+"""
+
+my_variable = "Esta es mi primer variable"
+
+#como tal, constante no existe en python porque toda variable puede ser modificada, pero podriamos usar la variable en MAYUSCULA para especificar la continuidad de dicha variable para evitar ser modificada.
+MI_CONSTANT = "No me cambies"
+
+my_string = "Soy una cadena de texto"
+my_other_string = 'Otra forma de crear la cadena de texto'
+my_int = 10
+my_float = 2.0
+my_bool = True
+my_bool = False
+
+print ("¡Hola, Python!"
